b2d3e05fb93d32958b15c871ab749a60125270f4
easybrizi <kiri4enkoalexandr@gmail.com>
2022-01-10 10:30:29 +0200

    feat: some agly solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+        const numbersToTwentyArr = ' one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(' ') //
+        const dozensArr = 'null null twenty thirty forty fifty sixty seventy eighty ninety'.split(' ')
+        return (n=== 0)? 'zero':
+               (n < 20) ?
+               numbersToTwentyArr[n].trim() :
+               (n < 100) ?
+               `${dozensArr[('' + n)[0]]} ${numbersToTwentyArr[('' + n)[1]]}`.trim():
+               (n < 1000 && +('' + n).slice(1) < 20) ?
+               `${numbersToTwentyArr[('' + n)[0]]} hundred ${numbersToTwentyArr[+('' + n).slice(1)]}`.trim():
+               `${numbersToTwentyArr[('' + n)[0]]} hundred ${dozensArr[('' + n)[1]]} ${numbersToTwentyArr[('' + n)[2]]}`.trim()
 }
